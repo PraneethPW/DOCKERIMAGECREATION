c1d588a8754866d97590faaab43aca7374ac03f7
Drake0112 <praneethreddy0112@gmail.com>
2025-12-01 05:52:25 +0530

    added all the files

---
+++ src/index.ts
@@ -0,0 +1,53 @@
+import "dotenv/config";
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma/client'
+import express from 'express'
+
+const connectionString = `${process.env.DATABASE_URL}`
+
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
+
+
+const app=express();
+const port = 3000;
+
+app.get("/" , async (req , res)=>{
+    const email = await req.body.email
+
+
+        await prisma.user.findFirst({
+            where : {
+                email 
+            }
+        })
+
+        res.json({
+            message : `DATA FETCHED WITH EMAIL ${email}`
+        })
+})
+
+app.post("/insert" , async (req , res)=>{
+    
+
+    const email = await req.body.email ; 
+    const password = await req.body.password;
+
+ const user_id =  await  prisma.user.create({
+        data:{
+            email : email, 
+            password : password
+        } , select:{
+            id : true 
+        }
+    })
+
+  res.json({
+      user_id
+  })
+})
+
+app.listen(port)
+console.log(`LISTINING ON PORT ${port}`)
+
+
